f1c91ad832659fd5752bf10e15679de3603099f5
helioalves <helio.alves@expretio.com>
2024-03-16 12:39:10 -0400

    chore: Remove api app

--- apps/api/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@umontreal/eslint-config/server.js"],
-};
--- apps/web-app/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export * from 'components/ui/button';
-export * from 'components/ui/table';
-export * from 'components/ui/counter';
-export * from 'components/ui/heading';
